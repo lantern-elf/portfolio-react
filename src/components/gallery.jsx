26ede46cddec94590c72ce748775bf40262d2b76
Lantern Elf <lantern4k@gmail.com>
2024-09-30 18:14:41 +0700

    gallery update

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,16 +1,20 @@
 import '../index.css'
 import './components.css'
 import './gallery.css'
+import artsImg from '../assets/arts.json'
 
 function Gallery() {
+    
     return(
         <>
             <div className="gallery">
-                <div><img className='arts' src="src/assets/imgs/Echoes of Wisdom.png" alt="" /></div>
-                <div><img className='arts' src="src/assets/imgs/Zelda unused design form master works.png" alt="" /></div>
-                <div><img className='arts' src="src/assets/imgs/new face.png" alt="" /></div>
-                <div><img className='arts' src="src/assets/imgs/lying in bed.png" alt="" /></div>
-                <div><img className='arts' src="src/assets/imgs/red-link for enni.png" alt="" /></div>
+                <div className='galleryWrapper'>
+                    {
+                        artsImg.map(arts => (
+                            <img className='arts' key={arts.id} src={arts.src} />
+                        ))
+                    }
+                </div>
             </div>
         </>
     )
