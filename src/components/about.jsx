48f36088845db2734103fd86b5fb14626df5eff4
Lantern Elf <lantern4k@gmail.com>
2024-10-09 18:31:48 +0700

    updating about and gallery

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -15,23 +15,19 @@ function About() {
                     </div>
                     <div className="column about">
                         <div className='aboutSess'>
-                            <div className='aboutHeader'>
-                                Name
-                            </div>
+                            <div className='aboutHeader'>Name</div>
                             Lantern Elf
                         </div>
                         <div className='aboutSess'>
-                            <div className='aboutHeader'>
-                                E-Mail
-                            </div>
+                            <div className='aboutHeader'>E-Mail</div>
                             lanternelf@gmail.com
                         </div>
                     </div>
                 </div>
                 <div className='contentSess2'>
                     <div className='contentText'>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta beatae iste quia porro obcaecati ex tempora, placeat veniam dolorem animi blanditiis accusamus a sapiente asperiores vero, eos fuga dignissimos quam.
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, atque perspiciatis. Earum ipsum commodi, consequatur porro, perspiciatis adipisci deserunt dignissimos nulla laborum in et, eos nostrum ipsa cumque atque hic?
+                        Greetings. I am <b>Lantern</b>, an <i>illustrator</i> and <i>programmer</i>.
+                        I specialize in creating illustrations, concept art, front-end web development, and game development.
                     </div>
                 </div>
             </div>
